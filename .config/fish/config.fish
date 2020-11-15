8c979d003f41368e681495b185a9c78dcef501aa
sekiya9311 <sekiya9311@gmail.com>
2020-11-16 01:33:25 +0900

    chore: 何かおかしい気がする

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,10 +3,5 @@ export LANG=ja_jp.utf8
 # alias ssh="mosh"
 alias g++="g++ -O2 -Wall"
 
-set PATH /Developer/flutter/bin $PATH
-set PATH /Users/sekiya9311/.nodebrew/current/bin $PATH
-
-set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
-
 export LSCOLORS=gxfxcxdxbxegedabagacad
 
