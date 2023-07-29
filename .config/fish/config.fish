a616f1ddc1ad7238a45ad2d3f6698a85b3a91090
sekiya9311 <sekiya9311@gmail.com>
2023-07-29 18:33:58 +0900

    feat: use zsh

--- .config/fish/config.fish
+++
@@ -1,8 +0,0 @@
-export LANG=ja_jp.UTF-8
-export LC_ALL=en_US.UTF-8
-
-# alias ssh="mosh"
-alias g++="g++ -O2 -Wall"
-
-export LSCOLORS=gxfxcxdxbxegedabagacad
-
---
+++ install-ubuntu.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+cp .gitconfig ../
+cp .p10k.zsh ../
+cp .tigrc ../
+cp .tmux.conf ../
+cp .vimrc ../
+cp .zshrc ../
+
+sudo apt update
+
+sudo apt install zsh
+sudo apt install tig
+sudo git clone https://github.com/tfutils/tfenv.git ~/.tfenv
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.4/install.sh | bash
+
+which zsh | sudo tee -a /etc/shells
+chsh -s $(which zsh)
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+cp .gitconfig ../
+cp .p10k.zsh ../
+cp .tigrc ../
+cp .tmux.conf ../
+cp .vimrc ../
+cp .zshrc ../
