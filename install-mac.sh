b88f64976a089feef7b9ba4b5904125a62a0ec84
sekiya9311 <sekiya9311@gmail.com>
2023-08-03 07:58:15 +0900

    wip

---
+++ install-mac.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+cp .gitconfig ../
+cp .tigrc ../
+cp .tmux.conf ../
+cp .vimrc ../
+cp .zshrc ../
+cp -r .config ../
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew install zsh
+brew install neovim
+brew install tig
+brew install mosh
+curl -sS https://starship.rs/install.sh | sh
+brew install tfenv
+brew install azure-cli
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.4/install.sh | bash
+
+which zsh | sudo tee -a /etc/shells
+chsh -s $(which zsh)
+
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+ cp .gitconfig ../
+ cp .tigrc ../
+ cp .tmux.conf ../
+ cp .vimrc ../
+ cp .zshrc ../
+ cp -r .config ../
+
