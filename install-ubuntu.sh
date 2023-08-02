2704a923f90c730d774e90423c4da8597e9cdfe5
sekiya9311 <sekiya9311@gmail.com>
2023-08-02 12:52:37 +0900

    add starship

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 cp .gitconfig ../
-cp .p10k.zsh ../
 cp .tigrc ../
 cp .tmux.conf ../
 cp .vimrc ../
@@ -14,23 +13,18 @@ sudo apt install zsh
 sudo apt install neovim
 sudo apt install tig
 sudo apt install mosh
+curl -sS https://starship.rs/install.sh | sh
 sudo git clone https://github.com/tfutils/tfenv.git ~/.tfenv
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.4/install.sh | bash
 
 which zsh | sudo tee -a /etc/shells
 chsh -s $(which zsh)
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
 cp .gitconfig ../
-cp .p10k.zsh ../
 cp .tigrc ../
 cp .tmux.conf ../
 cp .vimrc ../
