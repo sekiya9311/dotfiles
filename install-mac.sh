ea08a47a38ad3d195ba1106ecffea13b88a5820e
sekiya9311 <sekiya9311@gmail.com>
2023-09-20 11:06:02 +0900

    feat(tmux): add TPM

--- install-mac.sh
+++ install-mac.sh
@@ -28,6 +28,8 @@ chsh -s $(which zsh)
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
 ln -sf ${CURRENT}/.gitconfig $HOME/.gitconfig
 ln -sf ${CURRENT}/.tigrc $HOME/.tigrc
 ln -sf ${CURRENT}/.tmux.conf $HOME/.tmux.conf
--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -26,6 +26,8 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-sy
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
 ln -sf ${CURRENT}/.gitconfig $HOME/.gitconfig
 ln -sf ${CURRENT}/.tigrc $HOME/.tigrc
 ln -sf ${CURRENT}/.tmux.conf $HOME/.tmux.conf
