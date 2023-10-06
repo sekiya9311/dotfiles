87eaa854ef216e081ec59d0c99e64ae7c1dc9011
sekiya9311 <sekiya9311@gmail.com>
2023-10-06 18:42:48 +0900

    chore(nvim): add plugs

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -14,5 +14,9 @@ return require('packer').startup(function(use)
     requires = { 'nvim-tree/nvim-web-devicons', opt = true }
   }
 
+  use 'tpope/vim-obsession'
+
+  use 'github/copilot.vim'
+
 end)
 
