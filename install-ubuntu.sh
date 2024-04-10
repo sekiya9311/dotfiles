ad81687f7b647302d9411c150180f3250f4b8f11
sekiya9311 <sekiya9311@gmail.com>
2024-04-10 11:03:22 +0900

    chore: add eza install via script

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -2,6 +2,12 @@
 
 CURRENT=$(cd $(dirname $0); pwd)
 
+# for install eza ( https://github.com/eza-community/eza/blob/b9dfd61723d0b1e184954eeab93c1087cdf96ed8/INSTALL.md#debian-and-ubuntu 1)
+sudo mkdir -p /etc/apt/keyrings
+wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
+echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
+sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
+
 sudo apt update
 
 sudo apt install zsh
